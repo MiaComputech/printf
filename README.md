1c95f91571ba932ef09fae07bdbb7b51421df49b
MiaComputech <MiaComputechplc@gmail.com>
2022-07-12 20:57:58 +0300

    Task 0

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-printf repo
+ - printf
+
---
+++ functions.c
@@ -0,0 +1,139 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ *  print_c - prints character
+ *  @args: character argument
+ *  Return: number of characters
+ */
+int print_c(va_list args)
+{
+	int c;
+
+	c = va_arg(args, int);
+	return (_putchar(c));
+}
+/**
+ *print_s - prints a string
+ *@args: string  argument
+ *Return: number of characters
+ */
+int print_s(va_list args)
+{
+	int i, count = 0;
+	char *str;
+
+	i = 0;
+	str = va_arg(args, char*);
+	if (str == NULL)
+		str = "(null)";
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+		count++;
+	}
+	return (count);
+}
+/**
+ * print_percent - pass the percent sign
+ * @args: string  argument
+ * Return: return the percent sign
+ */
+int print_percent(va_list args)
+{
+	char *str;
+
+	str = "%";
+	if (va_arg(args, int) == *str)
+	{
+		return (*str);
+	}
+	return (*str);
+}
+
+/**
+ *  print_d - prints a decimal
+ *  @args: decimal argument
+ *  Return: counter
+ */
+int print_d(va_list args)
+{
+
+	unsigned int absolute, aux, countnum, count;
+	int n;
+
+	count = 0;
+	n = va_arg(args, int);
+	if (n < 0)
+	{
+		absolute = (n * -1);
+		count += _putchar('-');
+	}
+	else
+		absolute = n;
+
+	aux = absolute;
+	countnum = 1;
+	while (aux > 9)
+	{
+		aux /= 10;
+		countnum *= 10;
+	}
+	while (countnum >= 1)
+	{
+		count += _putchar(((absolute / countnum) % 10) + '0');
+		countnum /= 10;
+	}
+	return (count);
+}
+/**
+ *  print_i - prints integer
+ *  @args: integer argument
+ *  Return: the decimal function
+ */
+
+int print_i(va_list args)
+{
+	return (print_d(args));
+}
+
+/**
+ * print_bin - convert to binary
+ * @b: number in decinal
+ * Return: number of chars printed
+ */
+int print_b(va_list b)
+{
+	unsigned int len, powten, j, digit, n, num;
+	int count = 0;
+
+	n = va_arg(b, unsigned int);
+	if (n != 0)
+	{
+		num = n;
+		len = 0;
+		while (num != 0)
+		{
+			num /= 2;
+			len++;
+		}
+		powten = 1;
+		for (j = 1; j <= len - 1; j++)
+			powten *= 2;
+		for (j = 1; j <= len; j++)
+		{
+			digit = n / powten;
+			_putchar(digit + '0');
+			count++;
+			n -= digit * powten;
+			powten /= 2;
+		}
+	}
+	else
+	{
+		_putchar('0');
+		return (1);
+	}
+	return (count);
+}
